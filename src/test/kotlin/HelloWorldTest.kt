919577ce9b5e0a859338073405fa4971540ff364
Артур Сарбаев <a.sarbaev@quality-lab.ru>
2023-05-19 18:09:27 +0300

    Ex3

--- src/test/kotlin/HelloWorldTest.kt
+++ src/test/kotlin/HelloWorldTest.kt
@@ -1,2 +1,13 @@
-class HelloWorldTest {
+import io.restassured.RestAssured
+import org.junit.jupiter.api.Test
+
+class HelloWorldTest() {
+
+    @Test
+    fun testHelloWorld(){
+        val response = RestAssured
+            .get("https://playground.learnqa.ru/api/hello")
+            .andReturn()
+        response.prettyPrint()
+    }
 }
