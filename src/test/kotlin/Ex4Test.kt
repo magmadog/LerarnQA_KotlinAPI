9709eddd05e78e4a3aff2444aa6141df04425997
Артур Сарбаев <a.sarbaev@quality-lab.ru>
2023-05-19 18:11:34 +0300

    Completed Ex4

---
+++ src/test/kotlin/Ex4Test.kt
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured
+import org.junit.jupiter.api.Test
+
+class Ex4Test {
+
+    @Test
+    fun testGetText(){
+        val response = RestAssured
+            .get(" https://playground.learnqa.ru/api/get_text")
+            .andReturn()
+        response.prettyPrint()
+    }
+}
