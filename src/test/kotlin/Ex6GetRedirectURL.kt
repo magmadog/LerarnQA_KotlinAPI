7ce4a12e0c1504b92ba248df662ed981fbf0a734
Артур Сарбаев <a.sarbaev@quality-lab.ru>
2023-05-24 16:05:38 +0300

    Ex6 completed

---
+++ src/test/kotlin/Ex6GetRedirectURL.kt
@@ -0,0 +1,17 @@
+import io.restassured.RestAssured
+import org.junit.jupiter.api.Test
+
+class Ex6GetRedirectURL {
+
+    @Test
+    fun test(){
+        val response = RestAssured
+            .given()
+            .redirects()
+            .follow(false)
+            .get("https://playground.learnqa.ru/api/long_redirect")
+            .andReturn()
+
+        println("Redirecting url: ${response.headers["Location"].value}")
+    }
+}
